14bbf44f6de6e063dd2f6b892ffcdffe3856f5ac
cc <1183864793@qq.com>
2020-12-21 22:19:41 +0800

    start for ddpg

---
+++ DDPG/config.py
@@ -0,0 +1,10 @@
+import os, sys
+import os.path as osp
+# 数据和log文件地址
+DEFAULT_DATA_DIR = osp.join(osp.abspath(osp.dirname(osp.dirname(__file__))), 'data')
+# 是否在log文件名后加时间戳
+FORCE_DATESTAMP = False
+#使用网格法调参时， 是否使用shorthand
+DEFAULT_SHORTHAND = True
+# 使用网格调参时，等待多长时间再启动实验
+WAIT_BEFORE_LAUNCH = 5
---
+++ DDPG/utils.py
@@ -0,0 +1,2 @@
+def setup_logger_kwargs(exp_name=None, seed=None, data_dir=None, datestamp=False):
+    pass
